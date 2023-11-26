8ae819908b2dd77229500bd85062c7efdccaf319
dewang <2002thapa@gmail.com>
2023-11-25 16:54:10 -0800

    first time

---
+++ script.js
@@ -0,0 +1,10 @@
+// function showImage() {
+//     var imageContainer = document.getElementById("imageContainer");
+//     imageContainer.style.display = "block";
+//   }
+  
+//   function hideImage() {
+//     var imageContainer = document.getElementById("imageContainer");
+//     imageContainer.style.display = "none";
+//   }
+  
